2a6a174807b7826e1f1a6e4a6c5d0d17ab4f7d63
felixzh <felixzh2020@126.com>
2025-04-11 14:17:12 +0800

    kafka share group feature

---
+++ kafka-4.x-feature/src/main/java/com/felixzh/learning/KafkaQueueDemo.java
@@ -0,0 +1,31 @@
+package com.felixzh.learning;
+
+import org.apache.kafka.clients.CommonClientConfigs;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaShareConsumer;
+import org.apache.kafka.common.serialization.StringDeserializer;
+
+import java.time.Duration;
+import java.util.Collections;
+import java.util.Properties;
+
+public class KafkaQueueDemo {
+    public static void main(String[] args) {
+        Properties props = new Properties();
+        props.setProperty(CommonClientConfigs.BOOTSTRAP_SERVERS_CONFIG, "FelixZhnew:9092");
+        props.setProperty(CommonClientConfigs.GROUP_ID_CONFIG, "FelixZhQueue");
+
+        KafkaShareConsumer<String, String> kafkaShareConsumer =
+                new KafkaShareConsumer<>(props, new StringDeserializer(), new StringDeserializer());
+        kafkaShareConsumer.subscribe(Collections.singletonList("test"));
+
+        while (true) {
+            ConsumerRecords<String, String> records = kafkaShareConsumer.poll(Duration.ofMillis(100));
+            for (ConsumerRecord<String, String> record : records) {
+                System.out.println(record.toString());
+            }
+            kafkaShareConsumer.commitAsync();
+        }
+    }
+}
